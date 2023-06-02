f3b7c41ebf892be6c4ebe4d4d38f60c8a6b8ca33
Francisco Folli <francisco.folli@aluno.faculdadeimpacta.com.br>
2023-06-02 20:09:05 -0300

    email sender

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask,  render_template
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+# set gmail
+app.config['MAIL_USERNAME'] = 'victorkrajnovic@gmail.com'
+# set pass
+app.config['MAIL_PASSWORD'] = 'Fran4459993067'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 @app.route("/")
 def index():
@@ -14,6 +23,11 @@ def roupas():
 
 @app.route("/calcas")
 def calcas():
+    return render_template("calcas.html")
+
+@app.route("/email", methods = ['POST'])
+def email():
+    
     return render_template("calcas.html")
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,23 +1,41 @@
-document.addEventListener("DOMContentLoaded", function (event) 
-{
-    const showNavbar = (toggleId, navId, bodyId, headerId) => {
-        const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId),
-            bodypd = document.getElementById(bodyId),
-            headerpd = document.getElementById(headerId);
+var form = document.getElementById("form1")
 
-        // Valide se todas as variáveis existem
-        if (toggle && nav && bodypd && headerpd) {
-            toggle.addEventListener('click', () => {
-                // mostrar navbar
-                nav.classList.toggle('show');
-                // alterar ícone
-                toggle.classList.toggle('bx-x');
-                // adicionar padding ao corpo
-                bodypd.classList.toggle('body-pd');
-                // adicionar padding ao header
-                headerpd.classList.toggle('body-pd');
-            });
-        }
+form.onsubmit = function(event){
+
+event.preventDefault(); // Mata o evento padrão
+
+var dados = { } //Cria um JSON vazio, podemos usar o new Object()
+
+for (var i = 0; i < form.elements.length; i++){
+    
+    var input= form.elements[i];
+
+    if(input.email){
+        dados[input.email] = input.value;
+    } else if (input.whatsapp) {
+        dados[input.whatsapp] = input.value;
+    } else if (input.diasemana) {
+        dados[input.diasemana] = input.value;
+    } else if (input.horarios) {
+        dados[input.horarios] = input.value;
+    } else if (input.mensagem) {
+        dados[input.mensagem] = input.value;
     }
-})
+
+}
+
+// Constrói a requisição
+
+var xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange(… fazer algo …); // Veremos adiante
+
+xhr.open(form.method, form.action, true); // Abre a requisição
+
+xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+// Envia configurando o JSON como uma String/serializando
+
+xhr.send(JSON.stringify(dados));
+
+}
